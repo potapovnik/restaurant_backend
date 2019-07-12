6fbfef73eb5e791b40ef665a02c5da43f10c749b
potapovnr <potapovnr@gmail.com>
2019-07-12 13:20:25 +0300

    Дополнение конфигурации в бд.

--- db/src/main/java/ru/relex/restaurant/db/DbConfiguration.java
+++ db/src/main/java/ru/relex/restaurant/db/DbConfiguration.java
@@ -1,9 +1,13 @@
 package ru.relex.restaurant.db;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @PropertySource("db.properties")
+@EntityScan("ru.relex.restaurant.db.entity")
+@EnableJpaRepositories("ru.relex.restaurant.db.JpaRepository")
 public class DbConfiguration {
 }
