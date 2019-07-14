32f613db96feb0583180a7def3c3124a6b99abd3
potapovnr <potapovnr@gmail.com>
2019-07-14 15:20:44 +0300

    исправлен waiterOrdersRepository

--- db/src/main/java/ru/relex/restaurant/db/JpaRepository/WaiterOrdersRepository.java
+++ db/src/main/java/ru/relex/restaurant/db/JpaRepository/WaiterOrdersRepository.java
@@ -1,6 +1,7 @@
 package ru.relex.restaurant.db.JpaRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.relex.restaurant.db.entity.WaiterOrders;
 
-public interface WaiterOrdersRepository extends JpaRepository<WaiterOrdersRepository,Integer> {
+public interface WaiterOrdersRepository extends JpaRepository<WaiterOrders,Integer> {
 }
