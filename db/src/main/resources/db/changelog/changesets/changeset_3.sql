efe241aa992df827c6f1351cfa052148d19664ad
Maxim Mamontov <maximmamontov@mail.ru>
2019-07-16 16:35:45 +0300

    добавил changeset 3, который наполняет таблицы ingredients, ingredient_parts

---
+++ db/src/main/resources/db/changelog/changesets/changeset_3.sql
@@ -0,0 +1,8 @@
+INSERT INTO ingredients VALUES (1, 'Картофель', 'кг');
+INSERT INTO ingredients VALUES (2, 'Молоко', 'л');
+INSERT INTO ingredients VALUES (3, 'Яйца', 'шт');
+
+INSERT INTO ingredient_parts VALUES (1, 1, 30, '2019-08-08');
+INSERT INTO ingredient_parts VALUES (2, 1, 10, '2019-07-18');
+INSERT INTO ingredient_parts VALUES (3, 2, 1.2, '2019-07-18');
+INSERT INTO ingredient_parts VALUES (4, 2, 13, '2019-08-18');
