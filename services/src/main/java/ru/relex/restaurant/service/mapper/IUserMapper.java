b17a88d6941e96fef0b67c0be46a2cf2575ef3d2
Maxim Mamontov <maximmamontov@mail.ru>
2019-07-30 11:01:10 +0300

    сохранение hash'а пароля работает

--- services/src/main/java/ru/relex/restaurant/service/mapper/IUserMapper.java
+++ services/src/main/java/ru/relex/restaurant/service/mapper/IUserMapper.java
@@ -2,6 +2,9 @@ package ru.relex.restaurant.service.mapper;
 
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.relex.restaurant.db.entity.Users;
 import ru.relex.restaurant.service.DTO.UserDto;
 
@@ -9,14 +12,25 @@ import java.util.List;
 
 
 @Mapper(componentModel = "spring")
-public interface IUserMapper {
+public abstract class IUserMapper {
 
-  Users fromDto(UserDto userDto);
+  private PasswordEncoder passwordEncoder;
 
-  UserDto toDto(Users user);
+  @Autowired
+  public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+    this.passwordEncoder = passwordEncoder;
+  }
 
-  List<Users> fromDto(List<UserDto> userDtoList);
+  String mapPassword(UserDto dto) {
+    return passwordEncoder.encode(dto.getPassword());
+  }
 
-  List<UserDto> toDto(List<Users> usersList);
+  @Mapping(target = "password", expression = "java(mapPassword(userDto))")
+  public abstract Users fromDto(UserDto userDto);
 
+  public abstract UserDto toDto(Users user);
+
+  public abstract List<Users> fromDto(List<UserDto> userDtoList);
+
+  public abstract List<UserDto> toDto(List<Users> usersList);
 }
